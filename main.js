594c65954986f18434abced98a711f5cc39aca64
ximenasoto <119756954+ximenasoto@users.noreply.github.com>
2022-12-03 12:50:08 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//alert("Bienvenido a mi pagina");
+
+//Este es el codigo para la animacionn//
+
+let app= document.getElementById('typewriter');
+
+let typewriter = new Typewriter (app, {
+    loop: true,
+    delay: 75,
+});
+
+typewriter
+.pauseFor(2500)
+.typeString('Desarrollo sitios web y escribo artículos sobre programación')
+.pauseFor(200)
+.deleteChars(10)
+.start();
